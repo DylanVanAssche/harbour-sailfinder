a1683de3210424d46e9b859e2d7d09907ce9cd83
Dylan Van Assche <dylan.van.assche@protonmail.com>
2018-02-01 12:34:16 +0100

    Added Dutch translations

--- harbour-sailfinder.pro
+++ harbour-sailfinder.pro
@@ -101,7 +101,8 @@ CONFIG += sailfishapp_i18n \
 # planning to localize your app, remember to comment out the
 # following TRANSLATIONS line. And also do not forget to
 # modify the localized app name in the the .desktop file.
-TRANSLATIONS += translations/harbour-sailfinder.ts
+TRANSLATIONS += translations/harbour-sailfinder.ts \
+    translations/harbour-sailfinder-nl.ts
 
 HEADERS += \
     src/api.h \
---
+++ translations/harbour-sailfinder-nl.ts
@@ -0,0 +1,279 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nl">
+<context>
+    <name></name>
+    <message id="sailfinder-api-error">
+        <source>Tinder API couldn&apos;t complete your request</source>
+        <extracomment>Error shown to the user when the Tinder API failed to retrieve the requested data</extracomment>
+        <translation>De Tinder-API kon je verzoek niet verwerken</translation>
+    </message>
+    <message id="sailfinder-api-authentication-requested">
+        <source>Tinder token expired, refreshing now</source>
+        <translation>Tinder-token verlopen, wordt nu ververst</translation>
+    </message>
+    <message id="sailfinder-json-error">
+        <source>Invalid JSON data received, please try again later</source>
+        <extracomment>Error shown to the user when the data is invalid JSON data</extracomment>
+        <translation>Ongeldige JSON-gegevens ontvangen, probeer het later opnieuw</translation>
+    </message>
+    <message id="sailfinder-positioning-error">
+        <source>Positioning unavailable, check if location services are enabled</source>
+        <extracomment>Error shown to the user when an Positioning error occurs. The users could disabled GPS or an other error may be occured.</extracomment>
+        <translation>Positiebepaling niet beschikbaar, controleer of locatiediensten ingeschakeld zijn</translation>
+    </message>
+    <message id="sailfinder-oops">
+        <source>Oops!</source>
+        <translation>Oeps!</translation>
+    </message>
+    <message id="sailfinder-error">
+        <source>Something went wrong, please try again later</source>
+        <translation>Er ging iets mis, probeer het later opnieuw</translation>
+    </message>
+    <message id="sailfinder-about">
+        <source>About</source>
+        <translation>Over</translation>
+    </message>
+    <message id="sailfinder-recommendations">
+        <source>Recommendations</source>
+        <translation>Aanbevelingen</translation>
+    </message>
+    <message id="sailfinder-matches">
+        <source>Matches</source>
+        <extracomment>Header for matches</extracomment>
+        <translation>Matches</translation>
+    </message>
+    <message id="sailfinder-discoverable">
+        <source>Discoverable</source>
+        <translation>Ontdekbaar</translation>
+    </message>
+    <message id="sailfinder-discoverable-text">
+        <source>Disable discovery to hide your profile for other people. This has no effect on your current matches.</source>
+        <translation>Ontdekken uitschakelen zal je profiel verbergen voor andere gebruikers. Dit heeft geen effect op de matches die je al hebt.</translation>
+    </message>
+    <message id="sailfinder-discovery">
+        <source>Discovery</source>
+        <translation>Ontdekken</translation>
+    </message>
+    <message id="sailfinder-interested-in">
+        <source>Interested in</source>
+        <translation>Geïnteresseerd in</translation>
+    </message>
+    <message id="sailfinder-min-age">
+        <source>Min age</source>
+        <translation>Minimumleeftijd</translation>
+    </message>
+    <message id="sailfinder-search-radius">
+        <source>Search radius</source>
+        <translation>Zoekradius</translation>
+    </message>
+    <message id="sailfinder-radius-km">
+        <source>%L0 km</source>
+        <translation>%L0 km</translation>
+    </message>
+    <message id="sailfinder-logout">
+        <source>Logout</source>
+        <translation>Afmelden</translation>
+    </message>
+    <message id="sailfinder-profile">
+        <source>Profile</source>
+        <translation>Profiel</translation>
+    </message>
+    <message id="sailfinder-ssl-error">
+        <source>SSL error, please check your device is running with the correct date and time</source>
+        <extracomment>Error shown to the user when an SSL error occurs due a bad certificate or incorrect time settings.</extracomment>
+        <translation>SSL-fout, controleer of de juiste datum en tijd ingesteld zijn op je apparaat</translation>
+    </message>
+    <message id="sailfinder-max-age">
+        <source>Max age</source>
+        <translation>Maximumleeftijd</translation>
+    </message>
+    <message id="sailfinder-account">
+        <source>Account</source>
+        <translation>Account</translation>
+    </message>
+    <message id="sailfinder-female">
+        <source>Female</source>
+        <translation>Vrouw</translation>
+    </message>
+    <message id="sailfinder-male">
+        <source>Male</source>
+        <translation>Man</translation>
+    </message>
+    <message id="sailfinder-everyone">
+        <source>Everyone</source>
+        <translation>Iedereen</translation>
+    </message>
+    <message id="sailfinder-no-message">
+        <source>No messages yet, say hi!</source>
+        <extracomment>Text shown when no messages are retrieved in a conversation.</extracomment>
+        <translation>Nog geen berichten, zeg eens hallo!</translation>
+    </message>
+    <message id="sailfinder-new-match">
+        <source>New match!</source>
+        <translation>Nieuwe match!</translation>
+    </message>
+    <message id="sailfinder-new-match-hint">
+        <source>You have received a new match! Go say hi!</source>
+        <translation>Je hebt een nieuwe match! Zeg eens hallo!</translation>
+    </message>
+    <message id="sailfinder-remaining-characters">
+        <source>%L0/%L1</source>
+        <translation>%L0/%L1</translation>
+    </message>
+    <message id="sailfinder-bio-hint">
+        <source>Type your biography here</source>
+        <translation>Voer hier je biografie in</translation>
+    </message>
+    <message id="sailfinder-logout-error">
+        <source>Logout error, please try again later</source>
+        <extracomment>Error shown to the user when logging out of Facebook failed</extracomment>
+        <translation>Fout bij afmelden, probeer het later opnieuw</translation>
+    </message>
+    <message id="sailfinder-unmatching">
+        <source>Unmatching</source>
+        <translation>Match wordt verwijderd</translation>
+    </message>
+    <message id="sailfinder-unmatch">
+        <source>Unmatch</source>
+        <translation>Match verwijderen</translation>
+    </message>
+    <message id="sailfinder-not-connected">
+        <source>Network connection failure</source>
+        <translation>Netwerkverbindingsfout</translation>
+    </message>
+    <message id="sailfinder-version">
+        <source>About %0 V%1</source>
+        <translation>About %0 v%1</translation>
+    </message>
+    <message id="sailfinder-what-is">
+        <source>What&apos;s %0?</source>
+        <translation>Wat is %0?</translation>
+    </message>
+    <message id="sailfinder-what-is-text">
+        <source>%0 is an opensource application to have fun on Tinder with your Sailfish OS smartphone!</source>
+        <translation>%0 is een openbronapp om plezier te hebben op Tinder met je Sailfish OS-smartphone!</translation>
+    </message>
+    <message id="sailfinder-privacy-licensing">
+        <source>Privacy &amp; licensing</source>
+        <translation>Privacy &amp; licenties</translation>
+    </message>
+    <message id="sailfinder-privacy-licensing-text">
+        <source>%0 will never collect any personal information about the user, but this can&apos;t be guaranteed from any third-party company used in %0. This application is released under GPLv3. The source code and the license is available in the Github repo of %0.</source>
+        <translation>%0 zal nooit persoonlijke informatie over de gebruiker verzamelen, maar dit kan niet gegarandeerd worden van alle derde partijen die in %0 gebruikt worden. Deze toepassing wordt uitgegeven onder de GPLv3. De broncode en licentie zijn beschikbaar in de GitHub-pakketbron van %0.</translation>
+    </message>
+    <message id="sailfinder-disclaimer">
+        <source>Disclaimer</source>
+        <translation>Vrijwaring</translation>
+    </message>
+    <message id="sailfinder-disclaimer-text">
+        <source>%0 and it&apos;s contributors aren&apos;t related to %1 in any way and they can&apos;t be hold responsible for anything. You agree automatically with this disclaimer by using the application, contribute to it, ...</source>
+        <translation>%0 en bijdragers zijn niet gerelateerd aan %1, en kunnen nergens voor verantwoordelijk gehouden worden. Bij gebruik van of bijdragen aan deze app ga je automatisch akkoord met deze vrijwaring.</translation>
+    </message>
+    <message id="sailfinder-developer-source">
+        <source>Developer &amp; source code</source>
+        <translation>Ontwikkelaar &amp; broncode</translation>
+    </message>
+    <message id="sailfinder-donate-with">
+        <source>Donate with %0</source>
+        <translation>Doneren met %0</translation>
+    </message>
+    <message id="sailfinder-source">
+        <source>Source code</source>
+        <translation>Broncode</translation>
+    </message>
+    <message id="sailfinder-translations">
+        <source>Translations</source>
+        <translation>Vertalingen</translation>
+    </message>
+    <message id="sailfinder-translations-text">
+        <source>%0 can be translated into your language but for that we need your help! You can translate this app on %1</source>
+        <translation>%0 kan in je taal vertaald worden, maar daarvoor hebben we je hulp nodig! Je kan deze app vertalen op %1</translation>
+    </message>
+    <message id="sailfinder-translations-project">
+        <source>%0 project</source>
+        <translation>%0-project</translation>
+    </message>
+    <message id="sailfinder-powered-by">
+        <source>Powered by</source>
+        <translation>Met dank aan</translation>
+    </message>
+    <message id="sailfinder-icons">
+        <source>%0 icons</source>
+        <translation>%0-pictogrammen</translation>
+    </message>
+    <message id="sailfinder-out-of-recs">
+        <source>Exhausted!</source>
+        <translation>Uitgeput!</translation>
+    </message>
+    <message id="sailfinder-out-of-recs-text">
+        <source>Please come back later</source>
+        <translation>Kom later terug</translation>
+    </message>
+    <message id="sailfinder-new-matches">
+        <source>New matches!</source>
+        <translation>Nieuwe matches!</translation>
+    </message>
+    <message id="sailfinder-new-matches-hint">
+        <source>You have received %L0 new matches! Go say hi!</source>
+        <translation>Je hebt %L0 nieuwe matches! Zeg eens hallo!</translation>
+    </message>
+    <message id="sailfinder-no-matches">
+        <source>No matches!</source>
+        <translation>Geen matches!</translation>
+    </message>
+    <message id="sailfinder-no-matches-text">
+        <source>Swipe on some recommendations</source>
+        <translation>Veeg naar rechts of links op enkele aanbevelingen</translation>
+    </message>
+    <message id="sailfinder-messaging-placeholder">
+        <source>Say hi to %0!</source>
+        <translation>Zeg eens hallo tegen %0!</translation>
+    </message>
+    <message id="sailfinder-no-messages-text">
+        <source>No messages yet :-(</source>
+        <translation>Nog geen berichten ☹</translation>
+    </message>
+    <message id="sailfinder-no-messages-hint">
+        <source>Be the first one to start the conversation!</source>
+        <translation>Wees diegene die het gesprek start!</translation>
+    </message>
+    <message id="sailfinder-messaging-error">
+        <source>Messages couldn&apos;t be retrieved due missing profile information</source>
+        <extracomment>Error shown to the user when profile data wasn&apos;t succesfull retrieved. It&apos;s impossible then to get the messages between the user and it&apos;s matches.</extracomment>
+        <translation>Berichten konden niet worden opgehaald wegens ontbrekende profielinformatie</translation>
+    </message>
+    <message id="sailfinder-minutes-ago">
+        <source>%L0 minute(s) ago</source>
+        <translation>%L0 min geleden</translation>
+    </message>
+    <message id="sailfinder-hours-ago">
+        <source>%L0 hour(s) ago</source>
+        <translation>%L0 uur geleden</translation>
+    </message>
+    <message id="sailfinder-just-now">
+        <source>Just now</source>
+        <translation>Zojuist</translation>
+    </message>
+    <message id="sailfinder-new-message">
+        <source>New message!</source>
+        <translation>Nieuw bericht!</translation>
+    </message>
+    <message id="sailfinder-new-message-hint">
+        <source>You have received a new message!</source>
+        <translation>Je hebt een nieuw bericht ontvangen!</translation>
+    </message>
+    <message id="sailfinder-logging-in">
+        <source>Logging in</source>
+        <translation>Bezig met aanmelden</translation>
+    </message>
+    <message id="sailfinder-new-messages">
+        <source>New messages!</source>
+        <translation>Nieuwe berichten!</translation>
+    </message>
+    <message id="sailfinder-new-messages-hint">
+        <source>You have received %L0 new messages!</source>
+        <translation>Je hebt %L0 nieuwe berichten ontvangen!</translation>
+    </message>
+</context>
+</TS>
