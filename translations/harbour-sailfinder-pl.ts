f51fdc12814e383e3b5e4c19dfc90132fe691444
Dylan Van Assche <dylan.van.assche@protonmail.com>
2018-05-22 19:02:26 +0200

    Fixed launcher Harbour compatible

---
+++ translations/harbour-sailfinder-pl.ts
@@ -0,0 +1,319 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="pl" version="2.1">
+<context>
+    <name/>
+    <message id="sailfinder-api-error">
+        <source>Tinder API couldn&apos;t complete your request</source>
+        <extracomment>Error shown to the user when the Tinder API failed to retrieve the requested data</extracomment>
+        <translation>nder API nie może ukończyć Twojego zapytania</translation>
+    </message>
+    <message id="sailfinder-api-authentication-requested">
+        <source>Tinder token expired, refreshing now</source>
+        <translation>Token Tindera wygasł, odśwież teraz</translation>
+    </message>
+    <message id="sailfinder-json-error">
+        <source>Invalid JSON data received, please try again later</source>
+        <extracomment>Error shown to the user when the data is invalid JSON data</extracomment>
+        <translation>Otrzymano nieprawidłowe dane JSON, spróbuj ponownie później</translation>
+    </message>
+    <message id="sailfinder-positioning-error">
+        <source>Positioning unavailable, check if location services are enabled</source>
+        <extracomment>Error shown to the user when an Positioning error occurs. The users could disabled GPS or an other error may be occured.</extracomment>
+        <translation>Określanie pozycji niedostępne, sprawdź czy GPS jest włączony</translation>
+    </message>
+    <message id="sailfinder-oops">
+        <source>Oops!</source>
+        <translation>Ups!</translation>
+    </message>
+    <message id="sailfinder-error">
+        <source>Something went wrong, please try again later</source>
+        <translation>Coś poszło nie tak, spróbuj później</translation>
+    </message>
+    <message id="sailfinder-about">
+        <source>About</source>
+        <translation>O Sailfinder</translation>
+    </message>
+    <message id="sailfinder-recommendations">
+        <source>Recommendations</source>
+        <translation>Rekomendacje</translation>
+    </message>
+    <message id="sailfinder-matches">
+        <source>Matches</source>
+        <extracomment>Header for matches</extracomment>
+        <translation>Dopasowania</translation>
+    </message>
+    <message id="sailfinder-discoverable">
+        <source>Discoverable</source>
+        <translation>Wykrywalny</translation>
+    </message>
+    <message id="sailfinder-discoverable-text">
+        <source>Disable discovery to hide your profile for other people. This has no effect on your current matches.</source>
+        <translation>Wyłącz odkrycie aby ukryć twój profil dla innych ludzi. Nie ma to wpływu na obecne dopasowania.</translation>
+    </message>
+    <message id="sailfinder-discovery">
+        <source>Discovery</source>
+        <translation>Odkrycie</translation>
+    </message>
+    <message id="sailfinder-interested-in">
+        <source>Interested in</source>
+        <translation>Zainteresowany</translation>
+    </message>
+    <message id="sailfinder-min-age">
+        <source>Min age</source>
+        <translation>Min wiek</translation>
+    </message>
+    <message id="sailfinder-search-radius">
+        <source>Search radius</source>
+        <translation>Promień wyszukiwania</translation>
+    </message>
+    <message id="sailfinder-radius-km">
+        <source>%L0 km</source>
+        <translation>%L0 km</translation>
+    </message>
+    <message id="sailfinder-logout">
+        <source>Logout</source>
+        <translation>Wyloguj</translation>
+    </message>
+    <message id="sailfinder-profile">
+        <source>Profile</source>
+        <translation>Profil</translation>
+    </message>
+    <message id="sailfinder-ssl-error">
+        <source>SSL error, please check your device is running with the correct date and time</source>
+        <extracomment>Error shown to the user when an SSL error occurs due a bad certificate or incorrect time settings.</extracomment>
+        <translation>Błąd SSL, sprawdź, czy urządzenie działa z prawidłową datą i godziną</translation>
+    </message>
+    <message id="sailfinder-max-age">
+        <source>Max age</source>
+        <translation>Maks wiek</translation>
+    </message>
+    <message id="sailfinder-account">
+        <source>Account</source>
+        <translation>Konto</translation>
+    </message>
+    <message id="sailfinder-female">
+        <source>Female</source>
+        <translation>Kobieta</translation>
+    </message>
+    <message id="sailfinder-male">
+        <source>Male</source>
+        <translation>Mężczyzna</translation>
+    </message>
+    <message id="sailfinder-everyone">
+        <source>Everyone</source>
+        <translation>Wszyscy</translation>
+    </message>
+    <message id="sailfinder-no-message">
+        <source>No messages yet, say hi!</source>
+        <extracomment>Text shown when no messages are retrieved in a conversation.</extracomment>
+        <translation>Brak wiadomości, przywitaj się!</translation>
+    </message>
+    <message id="sailfinder-new-match">
+        <source>New match!</source>
+        <translation>Nowe dopasowanie!</translation>
+    </message>
+    <message id="sailfinder-new-match-hint">
+        <source>You have received a new match! Go say hi!</source>
+        <translation>Otrzymałeś nowe dopasowanie! Przywitaj się!</translation>
+    </message>
+    <message id="sailfinder-remaining-characters">
+        <source>%L0/%L1</source>
+        <translation>%L0/%L1</translation>
+    </message>
+    <message id="sailfinder-bio-hint">
+        <source>Type your biography here</source>
+        <translation>Napisz swój życiorys, tutaj</translation>
+    </message>
+    <message id="sailfinder-logout-error">
+        <source>Logout error, please try again later</source>
+        <extracomment>Error shown to the user when logging out of Facebook failed</extracomment>
+        <translation>Błąd wylogowania, spróbuj ponownie później</translation>
+    </message>
+    <message id="sailfinder-unmatching">
+        <source>Unmatching</source>
+        <translation>Usuwanie dopasowania</translation>
+    </message>
+    <message id="sailfinder-unmatch">
+        <source>Unmatch</source>
+        <translation>Usuń dopasowanie</translation>
+    </message>
+    <message id="sailfinder-not-connected">
+        <source>Network connection failure</source>
+        <translation>Błąd połączenia</translation>
+    </message>
+    <message id="sailfinder-version">
+        <source>About %0 V%1</source>
+        <translation>O %0 V%1</translation>
+    </message>
+    <message id="sailfinder-what-is">
+        <source>What&apos;s %0?</source>
+        <translation>Co jest %0?</translation>
+    </message>
+    <message id="sailfinder-what-is-text">
+        <source>%0 is an opensource application to have fun on Tinder with your Sailfish OS smartphone!</source>
+        <translation>%0 jest aplikacją open source do zabawy w Tinder przy użyciu twojego smartfona Sailfish OS!</translation>
+    </message>
+    <message id="sailfinder-privacy-licensing">
+        <source>Privacy &amp; licensing</source>
+        <translation>Prywatność i licencjonowanie</translation>
+    </message>
+    <message id="sailfinder-privacy-licensing-text">
+        <source>%0 will never collect any personal information about the user, but this can&apos;t be guaranteed from any third-party company used in %0. This application is released under GPLv3. The source code and the license is available in the Github repo of %0.</source>
+        <translation>%0 nigdy nie zbierze żadnych informacji osobistych o użytkowniku, ale nie można tego zagwarantować odmośnie żadnej firmy trzeciej używanej w %0. Ta aplikacja jest wydana na licencji GPLv3. Kod źródłowy i licencja są dostępne w repozytorium Github %0.</translation>
+    </message>
+    <message id="sailfinder-disclaimer">
+        <source>Disclaimer</source>
+        <translation>Zrzeczenie się</translation>
+    </message>
+    <message id="sailfinder-disclaimer-text">
+        <source>%0 and it&apos;s contributors aren&apos;t related to %1 in any way and they can&apos;t be hold responsible for anything. You agree automatically with this disclaimer by using the application, contribute to it, ...</source>
+        <translation>%0 i jego współtwórcy nie są w żaden sposób powiązani z %1 i nie mogą ponosić odpowiedzialności za cokolwiek. Automatycznie zgadzasz się z tym orzeczeiem poprzez używanie aplikacji, rozwijanie jej, ...</translation>
+    </message>
+    <message id="sailfinder-developer-source">
+        <source>Developer &amp; source code</source>
+        <translation>Twórca i kod źródłowy</translation>
+    </message>
+    <message id="sailfinder-donate-with">
+        <source>Donate with %0</source>
+        <translation>Wspomóż za pomocą %0</translation>
+    </message>
+    <message id="sailfinder-source">
+        <source>Source code</source>
+        <translation>Kod źródłowy</translation>
+    </message>
+    <message id="sailfinder-translations">
+        <source>Translations</source>
+        <translation>Tłumaczenia</translation>
+    </message>
+    <message id="sailfinder-translations-text">
+        <source>%0 can be translated into your language but for that we need your help! You can translate this app on %1</source>
+        <translation>%0 może zostać przetłumaczony na twój język, ale potrzebujemy twojej pomocy! Możesz przetłumaczyć aplikację na %1</translation>
+    </message>
+    <message id="sailfinder-translations-project">
+        <source>%0 project</source>
+        <translation>%0 projekt</translation>
+    </message>
+    <message id="sailfinder-powered-by">
+        <source>Powered by</source>
+        <translation>Zasilany przez</translation>
+    </message>
+    <message id="sailfinder-icons">
+        <source>%0 icons</source>
+        <translation>%0 ikon</translation>
+    </message>
+    <message id="sailfinder-out-of-recs">
+        <source>Exhausted!</source>
+        <translation>Wyczerpany!</translation>
+    </message>
+    <message id="sailfinder-out-of-recs-text">
+        <source>Please come back later</source>
+        <translation>Proszę wróć później</translation>
+    </message>
+    <message id="sailfinder-new-matches">
+        <source>New matches!</source>
+        <translation>Nowe dopasowania!</translation>
+    </message>
+    <message id="sailfinder-new-matches-hint">
+        <source>You have received %L0 new matches! Go say hi!</source>
+        <translation>Otrzymałeś %L0 nowych dopasowań! Przywitaj się!</translation>
+    </message>
+    <message id="sailfinder-no-matches">
+        <source>No matches!</source>
+        <translation>Brak dopasowań!</translation>
+    </message>
+    <message id="sailfinder-no-matches-text">
+        <source>Swipe on some recommendations</source>
+        <translation>Przesuń palcem nad poleceniami</translation>
+    </message>
+    <message id="sailfinder-messaging-placeholder">
+        <source>Say hi to %0!</source>
+        <translation>Przywitaj się z %0!</translation>
+    </message>
+    <message id="sailfinder-no-messages-text">
+        <source>No messages yet :-(</source>
+        <translation>Brak wiadomości :-(</translation>
+    </message>
+    <message id="sailfinder-no-messages-hint">
+        <source>Be the first one to start the conversation!</source>
+        <translation>Zacznij rozmowę jako pierwszy!</translation>
+    </message>
+    <message id="sailfinder-messaging-error">
+        <source>Messages couldn&apos;t be retrieved due missing profile information</source>
+        <extracomment>Error shown to the user when profile data wasn't succesfull retrieved. It's impossible then to get the messages between the user and it's matches.</extracomment>
+        <translation>Wiadomości nie mogą zostać przetworzone z powodu brakujących informacji w profilu</translation>
+    </message>
+    <message id="sailfinder-minutes-ago">
+        <source>%L0 minute(s) ago</source>
+        <translation>%L0 minut temu</translation>
+    </message>
+    <message id="sailfinder-hours-ago">
+        <source>%L0 hour(s) ago</source>
+        <translation>%L0 godzin temu</translation>
+    </message>
+    <message id="sailfinder-just-now">
+        <source>Just now</source>
+        <translation>Teraz</translation>
+    </message>
+    <message id="sailfinder-new-message">
+        <source>New message!</source>
+        <translation>Nowa wiadomość!</translation>
+    </message>
+    <message id="sailfinder-new-message-hint">
+        <source>You have received a new message!</source>
+        <translation>Otrzymałeś nową wiadomość!</translation>
+    </message>
+    <message id="sailfinder-logging-in">
+        <source>Logging in</source>
+        <translation>Logowanie</translation>
+    </message>
+    <message id="sailfinder-new-messages">
+        <source>New messages!</source>
+        <translation>Nowe wiadomości!</translation>
+    </message>
+    <message id="sailfinder-new-messages-hint">
+        <source>You have received %L0 new messages!</source>
+        <translation>Otrzymałeś %L0 nowych wiadomości!</translation>
+    </message>
+    <message id="sailfinder-timeout-error">
+        <source>Network timeout</source>
+        <extracomment>Error shown to the user when a network timeout was received</extracomment>
+        <translation>Błąd połączenia</translation>
+    </message>
+    <message id="sailfinder-optimizer">
+        <source>Optimizer</source>
+        <translation>Optymalizator</translation>
+    </message>
+    <message id="sailfinder-optimizer-text">
+        <source>The photo optimizer will automatically show your best photo&apos;s first on your profile.</source>
+        <translation>Optymalizator zdjęcia pokaże automatycznie twoje najlepsze zdjęcie, jako pierwsze.</translation>
+    </message>
+    <message id="sailfinder-logging-out">
+        <source>Logging out</source>
+        <translation>Wylogowywanie</translation>
+    </message>
+    <message id="sailfinder-removing-photo">
+        <source>Removing photo</source>
+        <translation>usuwanie zdjęcia</translation>
+    </message>
+    <message id="sailfinder-upload-photo">
+        <source>Upload photo</source>
+        <translation>Wgraj zdjęcie</translation>
+    </message>
+    <message id="sailfinder-upload-failed">
+        <source/>
+        <oldsource>Photo upload failed!</oldsource>
+        <translation>Wgrywanie zdjęcia zakończone niepowodzeniem!</translation>
+    </message>
+    <message id="sailfinder-login-facebook">
+        <source>Use Facebook login</source>
+        <translation>Zaloguj za pomocą Facebooka</translation>
+    </message>
+    <message id="sailfinder-login-phone">
+        <source>Use phone login</source>
+        <translation>Zaloguj za pomocą numeru telefonu</translation>
+    </message>
+    <message id="sailfinder-reload">
+        <source>Reload</source>
+        <translation>Przeładuj</translation>
+    </message>
+</context>
+</TS>
